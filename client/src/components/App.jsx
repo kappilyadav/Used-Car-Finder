d385f864bd9b48a4ee4856d00ec071a3209a203f
Kapil Yadav <ky22379@gmail.com>
2023-03-18 22:53:40 +0530

    button in-action

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,29 @@
 
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import Map from './Map';
-import React from 'react';
+
 
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [plot, setPlot] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:5000/api/data');
+      const responseData = await response.json();
+      setData(responseData);
+    };
+    fetchData();
+  }, []);
+
   
   return (
     <div className="App">
-      <button className='btn btn-primary btn-lg' type='submit'>Plot Data</button> 
-      <Map />
+      <button onClick={() => {setPlot(!plot)}} className='btn btn-primary btn-lg' type='submit'>Plot Data</button> 
+      {data && <Map apiKey={data.key} markers={data.array} plotMarkers={plot}/>}
     </div>
   );
 }
--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -4,53 +4,37 @@ import withScriptjs from 'react-google-maps/lib/withScriptjs';
 
 
 
-
-
-function MyMapComponent() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('http://localhost:5000/api/data');
-      const responseData = await response.json();
-      setData(responseData);
-    };
-    fetchData();
-  }, []);
-
+function MyMapComponent(props) {
   
-  
-  const MapWithAMarker = withScriptjs(withGoogleMap( (props) => (
+  const MapWithAMarker = withScriptjs(withGoogleMap( () => (
     <GoogleMap
       defaultZoom={6}
       defaultCenter={{ lat: 25.3953, lng: 79.1379 }}
     >
-  
-  <Marker position={{ lat: 25.3953, lng: 25.3953 }} />
 
     {/* display marker on map */}
-      {
-        data.array.map((entry, index) => (
-          <Marker key={index} position={{ lat: parseFloat(entry.Lat), lng: parseFloat(entry.Lng) }} />
-        ))
-      }
+
+      {props.plotMarkers && 
+      props.markers.map((marker, index) => (
+          <Marker key={index} position={{ lat: parseFloat(marker.Lat), lng: parseFloat(marker.Lng) }} />
+      ))}
       
     </GoogleMap>
   )));
 
 
 
-
   return (
-    data && <MapWithAMarker
-
-      googleMapURL={"https://maps.googleapis.com/maps/api/js?key=" + data.key}
+    
+    <MapWithAMarker
+      googleMapURL={"https://maps.googleapis.com/maps/api/js?key=" + props.apiKey}
       loadingElement={<div style={{ height: '100%' }} />}
       containerElement={<div style={{ height: '600px' }} />}
       mapElement={<div style={{ height: '100%' }} />}
     />
   );
 }
+
 
 export default MyMapComponent;
 
