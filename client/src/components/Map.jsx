1111ac6d3f14c78b62a2e214b2942e8c7f90c5f7
Kapil Yadav <ky22379@gmail.com>
2023-03-20 09:49:05 +0530

    Marker Info on-click add

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,31 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import React, { useState } from 'react';
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 import withScriptjs from 'react-google-maps/lib/withScriptjs';
 
 
 
 function MyMapComponent(props) {
-  
-  const MapWithAMarker = withScriptjs(withGoogleMap( () => (
+  const [markerId, setMarkerId] = useState(null);
+
+
+  function showInfoBox(marker) {
+
+    if (marker.id === markerId) {
+      return <InfoWindow onCloseClick={() => setMarkerId(false)}>
+        <div>
+          <h6>{marker.Name}</h6>
+          <p>{"Model:" + marker.Year}, {"KM:" + marker.Kilometers_Driven}, {marker.Fuel_Type}</p>
+        </div>
+      </InfoWindow>
+    }
+  }
+
+  const MapWithAMarker = withScriptjs(withGoogleMap(() => (
     <GoogleMap
-      defaultZoom={6}
+      defaultZoom={7}
       defaultCenter={{ lat: 25.3953, lng: 79.1379 }}
     >
 
-    {/* display marker on map */}
 
-      {props.plotMarkers && 
-      props.markers.map((marker, index) => (
-          <Marker key={index} position={{ lat: parseFloat(marker.Lat), lng: parseFloat(marker.Lng) }} />
-      ))}
-      
+      {/* display marker on map */}
+
+      {props.plotMarkers &&
+        props.markers.map((marker) => (
+
+          <Marker
+            key={marker.id}
+            id={marker.id}
+            position={{ lat: parseFloat(marker.Lat), lng: parseFloat(marker.Lng) }}
+            onClick={() => setMarkerId(marker.id)}
+          >
+
+            {showInfoBox(marker)}
+
+          </Marker>
+        ))}
+
     </GoogleMap>
   )));
 
 
 
   return (
-    
+
     <MapWithAMarker
       googleMapURL={"https://maps.googleapis.com/maps/api/js?key=" + props.apiKey}
       loadingElement={<div style={{ height: '100%' }} />}
